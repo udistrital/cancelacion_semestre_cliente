761228de6ca759fcd6182225d3926efa075db9a4
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-30 20:05:06 -0500

    Plantilla Inicial Estudiantes, twitter-boostrap

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
+  'myApp.home',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'ui.bootstrap'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
--- app/views/home/home_test.js
+++ app/views/home/home_test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.home module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.home'));
 
-  describe('view1 controller', function(){
+  describe('home controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
