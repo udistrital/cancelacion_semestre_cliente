f507673f10d2fcecfc54a0e6bd074663c474609e
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-10-11 03:17:14 -0500

    Nombres bonitos para estado y tipo

--- app/views/estudiante/ver/view.js
+++ app/views/estudiante/ver/view.js
@@ -9,6 +9,12 @@ angular.module('myApp.estudianteVer', ['ngRoute'])
     resolve: {
       resolvedCancelacionSemestre: ['CancelacionSemestreFactory', function(CancelacionSemestreFactory) {
         return CancelacionSemestreFactory.query()
+      }],
+      resolvedTipoCancelacion: ['TipoCancelacionFactory', function(TipoCancelacionFactory) {
+        return TipoCancelacionFactory.query()
+      }],
+      resolvedEstadoCancelacion: ['EstadoCancelacionFactory', function(EstadoCancelacionFactory) {
+        return EstadoCancelacionFactory.query()
       }]
     }
   });
@@ -16,18 +22,48 @@ angular.module('myApp.estudianteVer', ['ngRoute'])
 
 .controller('EstudianteVerCtrl', [
   '$scope',
+  '$filter',
   'resolvedCancelacionSemestre',
+  'resolvedTipoCancelacion',
+  'resolvedEstadoCancelacion',
   function(
     $scope,
-    resolvedCancelacionSemestre
+    $filter,
+    resolvedCancelacionSemestre,
+    resolvedTipoCancelacion,
+    resolvedEstadoCancelacion
   ) {
     $scope.m = {}
-    console.log(resolvedCancelacionSemestre);
-    window.a = resolvedCancelacionSemestre
+    angular.forEach(resolvedCancelacionSemestre, function(row, index) {
+      angular.forEach(row, function(value, key) {
+        var filtro = null
+        switch (key) {
+          case 'IdTipo':
+            filtro = $filter('filter')(resolvedTipoCancelacion, value)
+            break;
+          case 'IdEstado':
+            filtro = $filter('filter')(resolvedEstadoCancelacion, value)
+            break;
+          default:
+
+        }
+        if (filtro !== null) {
+          resolvedCancelacionSemestre[index][key] = filtro[0]
+        }
+      })
+    })
     $scope.m = resolvedCancelacionSemestre
   }
 ])
 
 .factory('CancelacionSemestreFactory', ['$resource', 'CONFIG', function($resource, CONFIG) {
   return $resource(CONFIG.WS_URL + '/cancelacion_semestre/:id');
+}])
+
+.factory('TipoCancelacionFactory', ['$resource', 'CONFIG', function($resource, CONFIG) {
+  return $resource(CONFIG.WS_URL + '/tipo_cancelacion_semestre/:id');
+}])
+
+.factory('EstadoCancelacionFactory', ['$resource', 'CONFIG', function($resource, CONFIG) {
+  return $resource(CONFIG.WS_URL + '/estado_cancelacion_semestre/:id');
 }])
